bacca46c9f90a787fd8523eec9a2cc3797448dda
RobRie <RobinRieger@gmx.de>
2026-05-19 15:36:14 +0200

    Update LightboxView.vue

--- src/components/LightboxView.vue
+++ src/components/LightboxView.vue
@@ -164,7 +164,7 @@ defineExpose({ cleanup })
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4 17v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 11l5 5 5-5M12 4v12" />
         </svg>
-        <span class="text-sm">{{ downloading ? 'Lädt…' : 'Bild speichern' }}</span>
+        <span class="text-sm">{{ downloading ? 'Lädt…' : 'Teilen' }}</span>
       </button>
       <div class="text-white/60 text-sm">
         {{ filename }}
